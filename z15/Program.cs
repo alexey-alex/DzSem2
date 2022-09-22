7efd3783ad47da1f50c8c5c27fc313831f9a5ad2
ALEXEY <alexey-sharcov@mail.ru>
2022-09-22 12:14:26 +0600

    Пытаюсь добавить папку z2

--- z15/Program.cs
+++ z15/Program.cs
@@ -1,8 +1,7 @@
-﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 */
-Console.WriteLine("Введите трёхзначное число");
-int num = Convert.ToInt32(Console.ReadLine());
-int number1 = (num % 100);
-int number2 = (number1 / 10);
-Console.WriteLine($"{number2}");
+int day = new Random().Next(1, 8);
+Console.WriteLine($"День номер {day}");
+if (day==7 || day == 6) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
 
